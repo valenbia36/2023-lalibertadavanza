a5350b5a68ebb21ff58b786c10223725be14d37e
Agustin Massieri <agmassieri00@gmail.com>
2023-09-29 17:37:32 -0300

    reset password email format

--- controllers/notifications.js
+++ controllers/notifications.js
@@ -3,18 +3,24 @@ const { sendEmail } = require('../utils/handleEmail');
 const sendResetPasswordEmail = async (req, res) => {
     const email = req.body.email;
     const token = req.body.token;
-    console.log(email)
-    console.log(token)
-
+    const userName = req.body.userName;
 
     try {
         const send_to = email;
         const sent_from = process.env.EMAIL_USER;
         const reply_to = email;
         const subject = "HeliApp - Reset Password";
+
         const message = `
-            <p>Link: localhost:3000/resetPassword</p>
-            <p>Token:${token}</p>
+        <p>¡Hola ${userName}!</p>
+        <p>Recibes este correo porque has solicitado restablecer tu contraseña en HeliApp.</p>
+        <p>Para completar este proceso, sigue los siguientes pasos:</p>
+        <ol>
+            <li>Haz clic en el siguiente enlace:</li>
+            <a href="www.localhost:3000/resetPassword">www.localhost:3000/resetPassword</a>
+            <li>Ingresa el siguiente token cuando se te solicite: <strong>${token}</strong></li>
+        </ol>
+        <p>Si no has solicitado restablecer tu contraseña, puedes ignorar este correo.</p>
         `;
 
         await sendEmail(subject, message, send_to, sent_from, reply_to);

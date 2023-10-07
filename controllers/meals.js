12ff78e85e01a5232903566bf07bb64650f28696
Pablodubosc <52469532+Pablodubosc@users.noreply.github.com>
2023-10-07 16:38:19 -0300

    fix to getmeals by month

--- controllers/meals.js
+++ controllers/meals.js
@@ -87,17 +87,48 @@ const getCaloriesByMonth = async (req, res) => {
         $regex: regexPattern,
       },
     });
-    const caloriesByDay = {};
 
-    meals.forEach((meal) => {
-      const mealDate = meal.date; // Extract the date portion
-      if (!caloriesByDay[mealDate]) {
-        caloriesByDay[mealDate] = 0;
+    const data = {};
+    meals.forEach(item => {
+      const date = item.date.substring(8, 10); // Extraer el día de la fecha
+      const calories = item.calories;
+    
+      // Si la fecha ya existe en el objeto, sumar las calorías
+      if (data[date]) {
+        data[date] += calories;
+      } else {
+        // Si la fecha no existe, crear una nueva entrada en el objeto
+        data[date] = calories;
       }
-      caloriesByDay[mealDate] += meal.calories;
     });
-    //res.json({ totalCalories });
-    res.send({ caloriesByDay });
+    
+    // Convertir el objeto en un arreglo de objetos con el formato deseado
+    const result = Object.entries(data).map(([date, calories]) => ({
+      date,
+      calorias: calories
+    }));
+    
+    result.sort((a, b) => {
+      const dateA = parseInt(a.date, 10);
+      const dateB = parseInt(b.date, 10);
+    
+      return dateA - dateB;
+    });
+    const caloriesMap = {};
+    result.forEach(item => {
+      caloriesMap[item.date] = item.calorias;
+    });
+    
+    // Crear un arreglo con todos los días del mes y establecer 0 para los que no estén presentes
+    const daysInMonth = 31; // Cambia esto según el mes
+    const resultWithAllDays = [];
+    
+    for (let day = 1; day <= daysInMonth; day++) {
+      const date = day.toString().padStart(2, '0'); // Formato "dd"
+      const calories = caloriesMap[date] || 0; // Establecer 0 si la fecha no está en el mapa
+      resultWithAllDays.push({ date, calorias: calories });
+    }
+    res.send({ resultWithAllDays })
   } catch (e) {
     console.log(e);
     handleHttpError(res, "ERROR_GET_CALORIES", 500);

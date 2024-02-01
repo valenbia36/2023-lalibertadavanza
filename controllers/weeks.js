7731e2f931f471cbf616a9bbffc769f3bb77b8eb
valenbia36 <vbianchi16@gmail.com>
2024-02-01 11:32:28 -0300

    clean dead code

--- controllers/weeks.js
+++ controllers/weeks.js
@@ -1,23 +1,6 @@
 const express = require("express");
 const { weekModel } = require("../models");
 
-/* const getWeek = async (req, res) => {
-  try {
-    const userId = req.params.id;
-    const weeks = await weekModel
-      .find({ userId: userId })
-      .populate({
-        path: "Monday.breakfast Monday.lunch Monday.snack Monday.dinner Tuesday.breakfast Tuesday.lunch Tuesday.snack Tuesday.dinner Wednesday.breakfast Wednesday.lunch Wednesday.snack Wednesday.dinner Thursday.breakfast Thursday.lunch Thursday.snack Thursday.dinner Friday.breakfast Friday.lunch Friday.snack Friday.dinner Saturday.breakfast Saturday.lunch Saturday.snack Saturday.dinner Sunday.breakfast Sunday.lunch Sunday.snack Sunday.dinner",
-      })
-      .exec();
-
-    res.status(200).json(weeks);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: error.message });
-  }
-}; */
-
 const getWeek = async (req, res) => {
   try {
     const userId = req.params.id;
@@ -27,8 +10,6 @@ const getWeek = async (req, res) => {
         path: "Monday.breakfast Monday.lunch Monday.snack Monday.dinner Tuesday.breakfast Tuesday.lunch Tuesday.snack Tuesday.dinner Wednesday.breakfast Wednesday.lunch Wednesday.snack Wednesday.dinner Thursday.breakfast Thursday.lunch Thursday.snack Thursday.dinner Friday.breakfast Friday.lunch Friday.snack Friday.dinner Saturday.breakfast Saturday.lunch Saturday.snack Saturday.dinner Sunday.breakfast Sunday.lunch Sunday.snack Sunday.dinner",
       })
       .exec();
-
-    // Mappear los resultados para incluir la última fecha de actualización
     const weeksWithLastUpdate = weeks.map((week) => ({
       ...week.toObject(),
       lastUpdate: week.updatedAt,
@@ -45,7 +26,6 @@ const saveWeek = async (req, res) => {
   try {
     const updatedPlan = req.body;
 
-    // Validar que userId está presente en el cuerpo de la solicitud
     if (!updatedPlan.userId) {
       return res
         .status(400)

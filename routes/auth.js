c10ee0157dda23c6250f7423096d2989ada299f1
valenbia36 <vbianchi16@gmail.com>
2024-06-29 12:52:51 -0300

    auth fix

--- routes/auth.js
+++ routes/auth.js
@@ -17,19 +17,9 @@ const { verifyToken } = require("../utils/handleJWT");
 const extractUserIdMiddleware = require("../utils/handleUserID");
 
 router.get("/users/", verifyToken, extractUserIdMiddleware, getUser); // este tampoco se usa en front... existe solo para chequear back
-router.get(
-  "/users/email/:email",
-  verifyToken,
-  extractUserIdMiddleware,
-  getUserByEmail
-); // creo que no se usa nunca desde el front.. borrarlo da problemas lo del mail en el a url pq habria que chequear q el token sea el mismo que el de la url
+router.get("/users/email/:email", getUserByEmail); // creo que no se usa nunca desde el front.. borrarlo da problemas lo del mail en el a url pq habria que chequear q el token sea el mismo que el de la url
 router.delete("/users/", verifyToken, extractUserIdMiddleware, deleteUser);
-router.put(
-  "/users/updatePassword/",
-  verifyToken,
-  extractUserIdMiddleware,
-  updateUserPassword
-);
+router.put("/users/updatePassword/", updateUserPassword);
 router.put("/users/", verifyToken, extractUserIdMiddleware, updateUser);
 router.post("/register/", validatorRegisterUser, registerController);
 router.post("/login/", validatorLoginUser, loginController);
